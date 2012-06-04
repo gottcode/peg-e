04c4ca704a9f46923a9dbec7661d3394f34c11ab
Graeme Gott <graeme@gottcode.org>
2012-06-04 17:29:18 -0400

    Add Dutch translation.

---
+++ translations/pege_nl.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../src/board.cpp" line="182"/>
+        <source>Success</source>
+        <translation>Gelukt</translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="182"/>
+        <source>Game Over</source>
+        <translation>Einde spel</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="49"/>
+        <source>Select application language:</source>
+        <translation>Selecteer programma taal:</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="52"/>
+        <source>&lt;System Language&gt;</source>
+        <translation>Systeem Taal</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="177"/>
+        <source>Note</source>
+        <translation>Noteren</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="177"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Herstarten van programma is nodig om van taal te veranderen.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="49"/>
+        <location filename="../src/window.cpp" line="240"/>
+        <source>Peg-E</source>
+        <translation>Peg-E</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="58"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Spel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="62"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nieuw</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="62"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="66"/>
+        <source>&amp;Restart Game</source>
+        <translation>&amp;Herstart Spel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="66"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="68"/>
+        <source>&amp;Details</source>
+        <translation>&amp;Informatie</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="74"/>
+        <source>Quit</source>
+        <translation>Einde</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="74"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="76"/>
+        <source>&amp;Move</source>
+        <translation>&amp;Verplaats</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="80"/>
+        <source>&amp;Undo</source>
+        <translation>&amp;Ongedaan maken</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="80"/>
+        <source>Ctrl+z</source>
+        <translation>Ctrl+z</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="86"/>
+        <source>&amp;Redo</source>
+        <translation>&amp;Opnieuw</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="86"/>
+        <source>Shift+Ctrl+Z</source>
+        <translation>Shift+Ctrl+Z</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="90"/>
+        <source>&amp;Appearance</source>
+        <translation>&amp;Uiterlijk</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="93"/>
+        <source>Blue</source>
+        <translation>Blauw</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="94"/>
+        <source>Charcoal</source>
+        <translation>Houtskool</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="95"/>
+        <source>Green</source>
+        <translation>Groen</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="96"/>
+        <source>Midnight</source>
+        <translation>Donkerzwart</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="97"/>
+        <source>Plum</source>
+        <translation>Paars</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="98"/>
+        <source>Red</source>
+        <translation>Rood</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="99"/>
+        <source>Yellow</source>
+        <translation>Geel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="117"/>
+        <source>Custom...</source>
+        <translation>Gebruik...</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="119"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Instelling</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="120"/>
+        <source>Application &amp;Language...</source>
+        <translation>Programma &amp;Taal...</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="122"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="123"/>
+        <source>&amp;About</source>
+        <translation>&amp;Over</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="124"/>
+        <source>About &amp;Qt</source>
+        <translation>Over &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="159"/>
+        <source>New Game</source>
+        <translation>Nieuw Spel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="163"/>
+        <location filename="../src/window.cpp" line="221"/>
+        <location filename="../src/window.cpp" line="307"/>
+        <source>Original</source>
+        <translation>Orgineel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="164"/>
+        <location filename="../src/window.cpp" line="214"/>
+        <location filename="../src/window.cpp" line="310"/>
+        <source>Branch</source>
+        <translation>Afdeling</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="165"/>
+        <location filename="../src/window.cpp" line="217"/>
+        <location filename="../src/window.cpp" line="313"/>
+        <source>Line</source>
+        <translation>Lijn</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="175"/>
+        <source>Random</source>
+        <translation>Willekeurige</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="184"/>
+        <source>Algorithm:</source>
+        <translation>Algoritme:</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="185"/>
+        <source>Difficulty:</source>
+        <translation>Moeilijkheid:</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="186"/>
+        <source>Seed:</source>
+        <translation>Seed:</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="202"/>
+        <source>Question</source>
+        <translation>Vraag</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="202"/>
+        <source>Do you want to restart?</source>
+        <translation>Wil je herstarten?</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="224"/>
+        <source>Details</source>
+        <translation>Informatie</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="224"/>
+        <source>&lt;p&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Difficulty:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Algoritme:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Moeilijkheid:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="237"/>
+        <source>About Peg-E</source>
+        <translation>Over Peg-E</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="241"/>
+        <source>Peg elimination game</source>
+        <translation>Peg eliminatie spel</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="242"/>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation>Auteursrecht &amp;copy;2012-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="243"/>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Uitgebracht onde de &lt;a href=%1&gt;GPL 3&lt;/a&gt; overeenkomst</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="244"/>
+        <source>Uses icons from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon theme</source>
+        <translation>Gebruik iconen van de &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon onderwerp</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="245"/>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation>Gebruikt onder de &lt;a href=%1&gt;OLGPL 3&lt;/a&gt; overeenkomst</translation>
+    </message>
+</context>
+</TS>
