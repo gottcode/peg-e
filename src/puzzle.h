91ba1ab0695e6cc0a2fd4218770cc6c6fa4c0299
Graeme Gott <graeme@gottcode.org>
2013-02-06 06:42:43 -0500

    Add documentation.

--- src/puzzle.h
+++ src/puzzle.h
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2009 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2009, 2013 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,50 +17,139 @@
  *
  ***********************************************************************/
 
-#ifndef PUZZLE_H
-#define PUZZLE_H
+#ifndef PEGE_PUZZLE_H
+#define PEGE_PUZZLE_H
 
 #include <QHash>
 #include <QPoint>
 #include <QSize>
 
-class Puzzle {
+/**
+ * Contains the layout of holes and pegs.
+ *
+ * This class generates and contains a board layout. When it generates a
+ * layout it tracks which holes are the furthest left and up, right and down
+ * so that the size of the layout can be quickly retrieved.
+ */
+class Puzzle
+{
 public:
+	/** Constructs an empty layout. */
 	Puzzle();
+
+	/** Destroys the layout. */
 	virtual ~Puzzle();
 
+	/** Returns the layout as a hash of points describing the holes. */
 	QHash<QPoint, bool> holes() const;
+
+	/** Returns the top left corner of the layout. */
 	QPoint position() const;
+
+	/** Returns the size of the layout. */
 	QSize size() const;
+
+	/**
+	 * Creates a layout.
+	 *
+	 * @param seed the seed passed to the random number generator
+	 * @param difficulty how hard of a layout to create
+	 */
 	void generate(int seed, int difficulty);
 
 protected:
+	/**
+	 * Creates a layout with a single group of pegs.
+	 *
+	 * @param pegs how many pegs to create
+	 */
 	virtual void generate(int pegs);
+
+	/**
+	 * Returns @c true if @p hole has no peg.
+	 *
+	 * @param hole the hole to check
+	 */
 	virtual bool isAvailable(const QPoint& hole) const;
+
+	/**
+	 * Randomly shuffles a list of pegs.
+	 *
+	 * @param pegs the pegs to shuffle
+	 */
 	virtual void shuffle(QList<QPoint>& pegs) const;
+
+	/**
+	 * Determines where to move next.
+	 *
+	 * @param start_hole the hole that is moved from
+	 * @param jumped_hole the hole between the start and end holes
+	 * @param end_hole the hole that is moved to
+	 */
 	bool findNextMove(const QPoint& start_hole, QPoint& jumped_hole, QPoint& end_hole);
+
+	/**
+	 * Creates a group of pegs.
+	 *
+	 * @param start initial peg
+	 * @param loops how many pegs to create
+	 */
 	QPoint findMoves(const QPoint& start, int loops);
+
+	/**
+	 * Returns whether or not the @p hole has a peg.
+	 *
+	 * @param hole the hole to check
+	 * @return @li @c 0 if the hole is empty
+	 *         @li @c 1 if the hole has a peg
+	 *         @li @c -1 if the hole doesn't exist
+	 */
 	int hasPeg(const QPoint& hole) const;
+
+	/**
+	 * Sets if the @p hole has a peg.
+	 *
+	 * @param hole the hole to specify
+	 * @param value what to put at the hole
+	 */
 	void setHasPeg(const QPoint& hole, bool value);
 
 private:
-	QHash<QPoint, bool> m_holes;
-	QList<QPoint> m_directions;
-	QPoint m_top_left;
-	QPoint m_bottom_right;
+	QHash<QPoint, bool> m_holes; /**< hash of holes */
+	QList<QPoint> m_directions; /**< list of directions to check */
+	QPoint m_top_left; /**< top left hole */
+	QPoint m_bottom_right; /**< bottom right hole */
 };
 
-
-class PuzzleBranch : public Puzzle {
+/** Puzzle that has branches between groups of pegs. */
+class PuzzleBranch : public Puzzle
+{
 protected:
-	virtual void generate(int pegs);
+	/**
+	 * Creates a layout with branches between the groups.
+	 *
+	 * @param pegs how many pegs to create
+	 */
+	void generate(int pegs);
 };
 
-
-class PuzzleLine : public Puzzle {
+/** Puzzle that doesn't reuse or shuffle any holes. */
+class PuzzleLine : public Puzzle
+{
 protected:
-	virtual bool isAvailable(const QPoint& hole) const;
-	virtual void shuffle(QList<QPoint>& pegs) const;
+	/**
+	 * Returns true if @p hole does not exist yet.
+	 *
+	 * @param hole the hole to check
+	 */
+	bool isAvailable(const QPoint& hole) const;
+
+	/**
+	 * Override parent function to prevent shuffling the pegs.
+	 *
+	 * @param pegs the pegs to shuffle
+	 */
+	void shuffle(QList<QPoint>& pegs) const;
 };
 
-#endif
+#endif // PEGE_PUZZLE_H
--- src/window.h
+++ src/window.h
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2009, 2012 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2009, 2012, 2013 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,41 +17,86 @@
  *
  ***********************************************************************/
 
-#ifndef WINDOW_H
-#define WINDOW_H
+#ifndef PEGE_WINDOW_H
+#define PEGE_WINDOW_H
+
+class Board;
 
 #include <QMainWindow>
 class QAction;
 class QActionGroup;
-class Board;
 
-class Window : public QMainWindow {
+/**
+ * Main window of the game.
+ *
+ * This is the main window. It defines the menubar and toolbar. It also
+ * controls starting a game, as well as fetching details of the running game.
+ */
+class Window : public QMainWindow
+{
 	Q_OBJECT
+
 public:
+	/** Constructs the main window. */
 	Window();
 
 protected:
-	virtual void closeEvent(QCloseEvent* event);
+	/** Override parent function to save window geometry. */
+	void closeEvent(QCloseEvent* event);
 
 private slots:
+	/**
+	 * Start a new game.
+	 *
+	 * Shows a dialog of options that the player can adjust.
+	 */
 	void newGame();
+
+	/** Restart the current game. */
 	void restartGame();
+
+	/** Show the current game details. */
 	void showDetails();
+
+	/**
+	 * Changes the appearance of the board.
+	 *
+	 * @param action the color to set the pegs
+	 */
 	void changeAppearance(QAction* action);
+
+	/**
+	 * Changes the appearance of the board.
+	 *
+	 * Shows a dialog that allows the player to choose the peg color.
+	 */
 	void changeAppearanceCustom();
+
+	/** Allows the player to change the application language. */
 	void setLocale();
+
+	/** Show the program details. */
 	void about();
 
 private:
+	/** Starts the previous game. */
 	void loadGame();
+
+	/**
+	 * Starts a new game.
+	 *
+	 * @param seed the seed passed to the random number generator
+	 * @param difficulty how hard of a layout to create
+	 * @param algorithm which Puzzle to use for the layout
+	 */
 	void startGame(int seed, int difficulty, int algorithm);
 
 private:
-	Board* m_board;
-	QActionGroup* m_colors;
-	int m_seed;
-	int m_difficulty;
-	int m_algorithm;
+	Board* m_board; /**< game board */
+	QActionGroup* m_colors; /**< actions to set peg color */
+	int m_seed; /**< seed passed to the random number generator */
+	int m_difficulty; /**< how hard of a layout was created */
+	int m_algorithm; /**< which Puzzle was used for the layout */
 };
 
-#endif
+#endif // PEGE_WINDOW_H
