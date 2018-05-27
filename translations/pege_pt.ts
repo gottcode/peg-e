1d8de34373ad59082dbcd201c2d4417f7f75fb28
Graeme Gott <graeme@gottcode.org>
2018-05-27 08:16:58 -0400

    Sync translations with Transifex.

--- translations/pege_pt.ts
+++ translations/pege_pt.ts
@@ -1,34 +1,34 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh">
+<TS version="2.1" language="pt">
 <context>
     <name>Board</name>
     <message>
         <source>Success</source>
-        <translation>恭喜你，你赢了！</translation>
+        <translation>Sucesso</translation>
     </message>
     <message>
         <source>Game Over</source>
-        <translation>游戏结束</translation>
+        <translation>Fim do Jogo</translation>
     </message>
 </context>
 <context>
     <name>LocaleDialog</name>
     <message>
         <source>Select application language:</source>
-        <translation>选择本游戏的语言:</translation>
+        <translation>Selecione o idioma da aplicação:</translation>
     </message>
     <message>
         <source>&lt;System Language&gt;</source>
-        <translation>&lt;系统语言&gt;</translation>
+        <translation>&lt;Idioma do Sistema&gt;</translation>
     </message>
     <message>
         <source>Note</source>
-        <translation>提示</translation>
+        <translation>Nota</translation>
     </message>
     <message>
         <source>Please restart this application for the change in language to take effect.</source>
-        <translation>请重启本程序使语言设置生效。</translation>
+        <translation>Por favor reinicie a aplicação para a alteração ao idioma surtir efeito.</translation>
     </message>
 </context>
 <context>
@@ -39,163 +39,163 @@
     </message>
     <message>
         <source>&amp;Game</source>
-        <translation>&amp;游戏</translation>
+        <translation>&amp;Jogo</translation>
     </message>
     <message>
         <source>&amp;New</source>
-        <translation>&amp;新游戏</translation>
+        <translation>&amp;Novo</translation>
     </message>
     <message>
         <source>&amp;Restart Game</source>
-        <translation>&amp;重新开始</translation>
+        <translation>&amp;Reiniciar Jogo</translation>
     </message>
     <message>
         <source>&amp;Details</source>
-        <translation>&amp;本局游戏详情</translation>
+        <translation>&amp;Detalhes</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation>退出程序</translation>
+        <translation>Sair</translation>
     </message>
     <message>
         <source>&amp;Move</source>
-        <translation>&amp;移动</translation>
+        <translation>&amp;Mover</translation>
     </message>
     <message>
         <source>&amp;Undo</source>
-        <translation>&amp;悔棋（撤销）</translation>
+        <translation>&amp;Desfazer</translation>
     </message>
     <message>
         <source>&amp;Redo</source>
-        <translation>重做</translation>
+        <translation>&amp;Refazer</translation>
     </message>
     <message>
         <source>&amp;Appearance</source>
-        <translation>棋子颜色</translation>
+        <translation>&amp;Aparência</translation>
     </message>
     <message>
         <source>Blue</source>
-        <translation>蓝</translation>
+        <translation>Azul</translation>
     </message>
     <message>
         <source>Charcoal</source>
-        <translation>碳灰</translation>
+        <translation>Carvão</translation>
     </message>
     <message>
         <source>Green</source>
-        <translation>绿</translation>
+        <translation>Verde</translation>
     </message>
     <message>
         <source>Midnight</source>
-        <translation>深蓝</translation>
+        <translation>Meia-Noite</translation>
     </message>
     <message>
         <source>Plum</source>
-        <translation>紫红</translation>
+        <translation>Ameixa</translation>
     </message>
     <message>
         <source>Red</source>
-        <translation>红</translation>
+        <translation>Vermelho</translation>
     </message>
     <message>
         <source>Yellow</source>
-        <translation>黄</translation>
+        <translation>Amarelo</translation>
     </message>
     <message>
         <source>Custom...</source>
-        <translation>自定义...</translation>
+        <translation>Personalizado...</translation>
     </message>
     <message>
         <source>&amp;Settings</source>
-        <translation>&amp;设置</translation>
+        <translation>D&amp;efinições</translation>
     </message>
     <message>
         <source>Application &amp;Language...</source>
-        <translation>程序 &amp;语言...</translation>
+        <translation>&amp;Idioma da Aplicação</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation>&amp;帮助</translation>
+        <translation>&amp;Ajuda</translation>
     </message>
     <message>
         <source>&amp;About</source>
-        <translation>&amp;关于</translation>
+        <translation>S&amp;obre</translation>
     </message>
     <message>
         <source>About &amp;Qt</source>
-        <translation>关于 &amp;Qt</translation>
+        <translation>Sobre o &amp;Qt</translation>
     </message>
     <message>
         <source>New Game</source>
-        <translation>新建游戏</translation>
+        <translation>Novo Jogo</translation>
     </message>
     <message>
         <source>Original</source>
-        <translation>原始</translation>
+        <translation>Original</translation>
     </message>
     <message>
         <source>Branch</source>
-        <translation>分支</translation>
+        <translation>Ramo</translation>
     </message>
     <message>
         <source>Line</source>
-        <translation>线性</translation>
+        <translation>Linha</translation>
     </message>
     <message>
         <source>Random</source>
-        <translation>随机</translation>
+        <translation>Aleatório</translation>
     </message>
     <message>
         <source>Algorithm:</source>
-        <translation>算法：</translation>
+        <translation>Algoritmo:</translation>
     </message>
     <message>
         <source>Difficulty:</source>
-        <translation>难度：</translation>
+        <translation>Dificuldade:</translation>
     </message>
     <message>
         <source>Seed:</source>
-        <translation>种子：</translation>
+        <translation>Semear:</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation>问题</translation>
+        <translation>Questão</translation>
     </message>
     <message>
         <source>Do you want to restart?</source>
-        <translation>你想要重新开始本局吗？</translation>
+        <translation>Quer reiniciar?</translation>
     </message>
     <message>
         <source>Details</source>
-        <translation>本局游戏详情</translation>
+        <translation>Detalhes</translation>
     </message>
     <message>
         <source>&lt;p&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Difficulty:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
-        <translation>&lt;p&gt;&lt;b&gt;算法：&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;难度：&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;排序：&lt;/b&gt; %L3&lt;/p&gt;</translation>
+        <translation>&lt;p&gt;&lt;b&gt;Algoritmo:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Dificuldade:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Semear:&lt;/b&gt; %L3&lt;/p&gt;</translation>
     </message>
     <message>
         <source>About Peg-E</source>
-        <translation>关于 Peg-E</translation>
+        <translation>Sobre o Peg-E</translation>
     </message>
     <message>
         <source>Peg elimination game</source>
-        <translation>一款孔明棋游戏</translation>
+        <translation>Jogo de eliminação Peg</translation>
     </message>
     <message>
         <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
-        <translation>版权所有 &amp;copy; 2009-%1 Graeme Gott</translation>
+        <translation>Direitos de autor &amp;copy; 2009-%1 Graeme Gott</translation>
     </message>
     <message>
         <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
-        <translation>基于 &lt;a href=%1&gt;GPL 3&lt;/a&gt; 协议发布</translation>
+        <translation>Lançado sob a licença  &lt;a href=%1&gt;GPL 3&lt;/a&gt;</translation>
     </message>
     <message>
         <source>Uses icons from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon theme</source>
-        <translation>使用 &lt;a href=%1&gt;Oxygen&lt;/a&gt; 风格图标</translation>
+        <translation>Usa ícones do tema de ícones &lt;a href=%1&gt;Oxygen&lt;/a&gt;</translation>
     </message>
     <message>
         <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
-        <translation>在 &lt;a href=%1&gt;LGPL 3&lt;/a&gt; 协议下授权用户使用</translation>
+        <translation>Usado sob a licença &lt;a href=%1&gt;LGPL 3&lt;/a&gt;</translation>
     </message>
 </context>
 </TS>
