c82ef60c4c060bd49ea5f449aa445fdba895c24c
Graeme Gott <graeme@gottcode.org>
2021-02-19 13:08:53 -0500

    Add Swedish translation.

---
+++ translations/pege_sv.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Success</source>
+        <translation>Succé</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>Slutspelat</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Välj programspråk:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Systemspråk&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Notis</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Starta om programmet för att byta språk.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Peg-E</source>
+        <translation>Peg-E</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Spel</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nytt</translation>
+    </message>
+    <message>
+        <source>&amp;Restart Game</source>
+        <translation>&amp;Starta om spelet</translation>
+    </message>
+    <message>
+        <source>&amp;Details</source>
+        <translation>&amp;Detaljer</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <source>&amp;Move</source>
+        <translation>&amp;Flytta</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Ångra</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;Upprepa</translation>
+    </message>
+    <message>
+        <source>&amp;Appearance</source>
+        <translation>&amp;Utseende</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>Blå</translation>
+    </message>
+    <message>
+        <source>Charcoal</source>
+        <translation>Kol</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>Grön</translation>
+    </message>
+    <message>
+        <source>Midnight</source>
+        <translation>Midnatt</translation>
+    </message>
+    <message>
+        <source>Plum</source>
+        <translation>Plommon</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>Röd</translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation>Gul</translation>
+    </message>
+    <message>
+        <source>Custom...</source>
+        <translation>Anpassa...</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Inställningar</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>Program&amp;språk...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Om &amp;Qt</translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation>Nytt spel</translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation>Original</translation>
+    </message>
+    <message>
+        <source>Branch</source>
+        <translation>Gren</translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation>Linje</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>Slumpmässigt</translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation>Algoritm:</translation>
+    </message>
+    <message>
+        <source>Difficulty:</source>
+        <translation>Svårighet:</translation>
+    </message>
+    <message>
+        <source>Seed:</source>
+        <translation>Slumptal:</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>Fråga</translation>
+    </message>
+    <message>
+        <source>Do you want to restart?</source>
+        <translation>Vill du starta om?</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Detaljer</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Difficulty:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;Algoritm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Svårighet:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Slumptal:&lt;/b&gt; %L3&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>About Peg-E</source>
+        <translation>Om Peg-E</translation>
+    </message>
+    <message>
+        <source>Peg elimination game</source>
+        <translation>Peggelimineringsspel</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation>Copyright &amp;copy; 2009-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Publicerat under &lt;a href=%1&gt;GPL 3&lt;/a&gt;- licensen</translation>
+    </message>
+    <message>
+        <source>Uses icons from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon theme</source>
+        <translation>Använder ikoner från &lt;a href=%1&gt;Oxygen&lt;/a&gt; ikontema</translation>
+    </message>
+    <message>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation>Använt under &lt;a href=%1&gt;LGPL 3&lt;/a&gt; -licensen</translation>
+    </message>
+</context>
+</TS>
