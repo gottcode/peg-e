66820d94674ede856565a6d5310cc381670a72f0
Graeme Gott <graeme@gottcode.org>
2021-04-12 16:10:29 -0400

    Require Qt 5.12

--- src/main.cpp
+++ src/main.cpp
@@ -76,12 +76,11 @@ int main(int argc, char** argv)
 	// Set up icons
 #if defined(Q_OS_MAC) || defined(Q_OS_WIN)
 	QIcon::setThemeName("hicolor");
-#elif (QT_VERSION >= QT_VERSION_CHECK(5,12,0))
-	QIcon::setFallbackThemeName("hicolor");
 #else
-	if (QIcon::themeName().isEmpty()) {
-		QIcon::setThemeName("hicolor");
+	if (QIcon::themeName() == "hicolor") {
+		QIcon::setThemeName("Hicolor");
 	}
+	QIcon::setFallbackThemeName("hicolor");
 #endif
 
 	Window window;
