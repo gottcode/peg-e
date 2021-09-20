aad7a7192f7b876aa3bbf4f8cdcb7ce884bc7dea
Graeme Gott <graeme@gottcode.org>
2021-09-20 17:58:51 -0400

    Sync translations with Transifex

---
+++ translations/pege_eo.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="eo">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Success</source>
+        <translation>Sukceson!</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>Malsukceson</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Elektu lingvon de programo:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Language&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Noto</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Bonvolu rekomencu ĉi tiun programon, tial la lingvo sanĝiĝas.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Peg-E</source>
+        <translation>Peg-E</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>Ludo</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>Nova</translation>
+    </message>
+    <message>
+        <source>&amp;Restart Game</source>
+        <translation>Rekomenci la ludon</translation>
+    </message>
+    <message>
+        <source>&amp;Details</source>
+        <translation>Detaloj</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Fini</translation>
+    </message>
+    <message>
+        <source>&amp;Move</source>
+        <translation>Movi</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>Refari</translation>
+    </message>
+    <message>
+        <source>&amp;Appearance</source>
+        <translation>Aspekto</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>Blua</translation>
+    </message>
+    <message>
+        <source>Charcoal</source>
+        <translation>Lignokarbo</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>Verda</translation>
+    </message>
+    <message>
+        <source>Midnight</source>
+        <translation>Mezonokta</translation>
+    </message>
+    <message>
+        <source>Plum</source>
+        <translation>Pruna</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>Roĝa</translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation>Flava</translation>
+    </message>
+    <message>
+        <source>Custom...</source>
+        <translation>Kotimo</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>Agordoj</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>Lingvo de programo</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>Helpo</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>Pri</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation>Nova ludo</translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Branch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>Hazarda</translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Difficulty:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Seed:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you want to restart?</source>
+        <translation>Ĉu vi volas rekomenci</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Difficulty:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Peg-E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Peg elimination game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uses icons from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/pege_te.ts
@@ -0,0 +1,201 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="te">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Success</source>
+        <translation>విజయం</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>ఆట సమాప్తం</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>అనువర్తన భాషను ఎంచుకోండి:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Language&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>గమనిక</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>భాషలో మార్పు అమలులోకి రావడానికి దయచేసి ఈ అనువర్తనాన్ని పునఃప్రారంభించండి.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Peg-E</source>
+        <translation>పెగ్-ఇ</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;ఆట</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;కొత్త</translation>
+    </message>
+    <message>
+        <source>&amp;Restart Game</source>
+        <translation>&amp;ఆటను పునఃప్రారంభించు</translation>
+    </message>
+    <message>
+        <source>&amp;Details</source>
+        <translation>&amp;వివరాలు</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>నిష్క్రమించు</translation>
+    </message>
+    <message>
+        <source>&amp;Move</source>
+        <translation>&amp;కదులు</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;అన్ డూ</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;రీడు</translation>
+    </message>
+    <message>
+        <source>&amp;Appearance</source>
+        <translation>&amp;స్వరూపం</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>నీలం</translation>
+    </message>
+    <message>
+        <source>Charcoal</source>
+        <translation>బొగ్గు</translation>
+    </message>
+    <message>
+        <source>Green</source>
+        <translation>ఆకుపచ్చ</translation>
+    </message>
+    <message>
+        <source>Midnight</source>
+        <translation>అర్ధరాత్రి</translation>
+    </message>
+    <message>
+        <source>Plum</source>
+        <translation>ప్లం</translation>
+    </message>
+    <message>
+        <source>Red</source>
+        <translation>ఎరుపు</translation>
+    </message>
+    <message>
+        <source>Yellow</source>
+        <translation>పసుపు</translation>
+    </message>
+    <message>
+        <source>Custom...</source>
+        <translation>కస్టమ్...</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;సెట్టింగులు</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>అనువర్తనం &amp;భాష...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;సహాయం</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;గురించి</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>&amp;Qt గురించి</translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation>కొత్త ఆట</translation>
+    </message>
+    <message>
+        <source>Original</source>
+        <translation>అసలైనది</translation>
+    </message>
+    <message>
+        <source>Branch</source>
+        <translation>శాఖ</translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation>గీత</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>పద్ధతి లేకుండా</translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation>అల్గోరిథం</translation>
+    </message>
+    <message>
+        <source>Difficulty:</source>
+        <translation>కష్టం</translation>
+    </message>
+    <message>
+        <source>Seed:</source>
+        <translation>విత్తనం:</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>ప్రశ్న</translation>
+    </message>
+    <message>
+        <source>Do you want to restart?</source>
+        <translation>మీరు పునఃప్రారంభించాలనుకుంటున్నారా?</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>వివరాలు</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Difficulty:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;అల్గోరిథం:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;కష్ట్టం:&lt;/b&gt; %2&lt;br&gt;&lt;b&gt;విత్తనం:&lt;/b&gt; %L3&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>About Peg-E</source>
+        <translation>పెగ్-ఇ గురించి</translation>
+    </message>
+    <message>
+        <source>Peg elimination game</source>
+        <translation>పెగ్ తొలగించే ఆట</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation>కాపీరైట్ &amp;కాపీ; 2009-%1 గ్రేమ్ గాట్</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>&lt;a href=%1&gt;GPL3&lt;/a&gt; లైసెన్స్ కింద విడుదల చేయబడింది</translation>
+    </message>
+    <message>
+        <source>Uses icons from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; icon theme</source>
+        <translation>&lt;a href=%1&gt;ఆక్స్సిజన్&lt;/a&gt; చిహ్నం థీమ్ నుండి చిహ్నాలను ఉపయోగిస్తుంది</translation>
+    </message>
+    <message>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation>&lt;a href=%1&gt;LGPL3&lt;/a&gt; లైసెన్స్ కింద ఉపయోగించబడినది</translation>
+    </message>
+</context>
+</TS>
